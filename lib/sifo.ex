e96fe5f6fe0bde3a4b12e502e74564f714e1043e
Chris Parker <mrcsparker@gmail.com>
2023-08-28 21:57:23 -0500

    Added a mutable ref for refresh_all

--- lib/sifo.ex
+++ lib/sifo.ex
@@ -7,8 +7,8 @@ defmodule Sifo do
     otp_app: :sifo,
     crate: :sifo
 
-  def add(_arg1, _arg2), do: :erlang.nif_error(:nif_not_loaded)
   def sys(), do: :erlang.nif_error(:nif_not_loaded)
+  def refresh_all(_), do: :erlang.nif_error(:nif_not_loaded)
   def available_memory(_), do: :erlang.nif_error(:nif_not_loaded)
   def total_memory(_), do: :erlang.nif_error(:nif_not_loaded)
 
--- native/sifo/src/lib.rs
+++ native/sifo/src/lib.rs
@@ -1,35 +1,36 @@
 use rustler::ResourceArc;
-use std::sync::Arc;
+use std::sync::Mutex;
 use sysinfo::{System, SystemExt};
 
-pub struct SystemResource(System);
+pub struct SystemResource(Mutex<System>);
 
 #[rustler::nif]
 fn sys() -> ResourceArc<SystemResource> {
-    let mut s = System::new_all();
-    s.refresh_all();
-    let system_resource = SystemResource(s);
-    ResourceArc::new(system_resource)
+    ResourceArc::new(SystemResource(Mutex::new(System::new_all())))
+}
+
+#[rustler::nif]
+fn refresh_all(resource: ResourceArc<SystemResource>) -> ResourceArc<SystemResource> {
+    {
+        let mut s = resource.0.lock().unwrap();
+        s.refresh_all();
+    }
+    resource
 }
 
 #[rustler::nif]
 fn available_memory(resource: ResourceArc<SystemResource>) -> u64 {
-    resource.0.available_memory()
+    resource.0.lock().unwrap().available_memory()
 }
 
 #[rustler::nif]
 fn total_memory(resource: ResourceArc<SystemResource>) -> u64 {
-    resource.0.total_memory()
-}
-
-#[rustler::nif]
-fn add(a: i64, b: i64) -> i64 {
-    a + b
+    resource.0.lock().unwrap().total_memory()
 }
 
 rustler::init!(
     "Elixir.Sifo",
-    [add, sys, available_memory, total_memory],
+    [sys, refresh_all, available_memory, total_memory],
     load = load
 );
 
